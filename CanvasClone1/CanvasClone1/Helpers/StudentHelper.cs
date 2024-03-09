e66125d7c5b92af58ce596a183ed11e349ccc1ca
Juan Morales <juanesmoralesd@gmail.com>
2024-03-09 10:29:39 -0500

    Added StudentHelper and StudentService to support creating student records in a centralized list of students

---
+++ CanvasClone1/CanvasClone1/Helpers/StudentHelper.cs
@@ -0,0 +1,49 @@
+﻿using Library.CanvasClone1.Models;
+using Library.CanvasClone1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanvasClone1.Helpers
+{
+    internal class StudentHelper
+    {
+        private StudentService studentService = new StudentService();
+        public void CreateStudent()
+        {
+            Console.WriteLine("What is the name of the student?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is the id of the student?");
+            var id = Console.ReadLine();
+            Console.WriteLine("What is the classification of the student? [(F)reshman, S(O)phomore, (J)unior, (S)enior]");
+            var classification = Console.ReadLine() ?? string.Empty;
+            StudentClass classEnum = StudentClass.Freshman;
+
+            if (classification.Equals("O", StringComparison.InvariantCultureIgnoreCase))
+            {
+                classEnum = StudentClass.Sophomore;
+            }
+            else if (classification.Equals("J", StringComparison.InvariantCultureIgnoreCase))
+            {
+                classEnum = StudentClass.Junior;
+            }
+            else if (classification.Equals("S", StringComparison.InvariantCultureIgnoreCase))
+            {
+                classEnum = StudentClass.Senior;
+            }
+
+            var student = new Person
+            {
+                Name = name ?? string.Empty,
+                ID = int.Parse(id ?? "0"),
+                Classification = classEnum
+            };
+
+            studentService.Add(student);
+
+            studentService.studentList.ForEach(Console.WriteLine);
+        }
+    }
+}
