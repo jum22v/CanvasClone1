42de7fc1bbbeda8fa7660373dd9f6797cc44440b
Juan Morales <juanesmoralesd@gmail.com>
2024-03-21 15:16:54 -0400

    Revert "Added AddCourse func"
    This reverts commit 4182b5579eb3e9e06b17eae6b75c711ce0de7fda.

--- CanvasClone1/CanvasClone1/Program.cs
+++ CanvasClone1/CanvasClone1/Program.cs
@@ -9,7 +9,7 @@ namespace MyApp
         static void Main(string[] args)
         {
             var studentHelper = new StudentHelper();
-            var courseHelper = new CourseHelper();
+
             bool cont = true;
 
             while (cont)
@@ -17,8 +17,7 @@ namespace MyApp
                 Console.WriteLine("1. Add a student");
                 Console.WriteLine("2. List all enrolled students");
                 Console.WriteLine("3. Search for a student");
-                Console.WriteLine("4. Add a new course");
-                Console.WriteLine("5. Exit");
+                Console.WriteLine("4. Exit");
                 var input = Console.ReadLine();
                 if (int.TryParse(input, out int result))
                 {
@@ -35,9 +34,6 @@ namespace MyApp
                     {
                         studentHelper.SearchStudents();
                     } else if (result == 4)
-                    {
-                        courseHelper.CreateCourse();
-                    } else if (result == 5)
                     {
                         cont = false;
                     }
--- CanvasClone1/Library.CanvasClone1/Models/Course.cs
+++ CanvasClone1/Library.CanvasClone1/Models/Course.cs
@@ -23,10 +23,5 @@ namespace Library.CanvasClone1.Models
             Assignments = new List<Assignment>();
             Modules = new List<Module>();
         }
-
-        public override string ToString()
-        {
-            return $"{Code} - {Name}";
-        }
     }
 }
