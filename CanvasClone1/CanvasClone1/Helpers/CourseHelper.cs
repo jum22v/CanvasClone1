aa84d60dc8beaef47b322e542dd9579a5af30867
Juan Morales <juanesmoralesd@gmail.com>
2024-03-21 15:31:40 -0400

    Added Create Course func frfr

---
+++ CanvasClone1/CanvasClone1/Helpers/CourseHelper.cs
@@ -0,0 +1,36 @@
+﻿using Library.CanvasClone1.Models;
+using Library.CanvasClone1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanvasClone1.Helpers
+{
+    public class CourseHelper
+    {
+        private CourseService courseService = new CourseService();
+
+        public void CreateCourse()
+        {
+            Console.WriteLine("What is the name of the course?");
+            var name = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("What is the code of the course?");
+            var code = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("What is the description of the course?");
+            var description = Console.ReadLine() ?? string.Empty;
+
+            var course = new Course
+            {
+                Name = name,
+                Code = code,
+                Description = description
+            };
+
+            courseService.Add(course);
+
+            courseService.courseList.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ CanvasClone1/Library.CanvasClone1/Services/CourseService.cs
@@ -0,0 +1,19 @@
+﻿using Library.CanvasClone1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.CanvasClone1.Services
+{
+    public class CourseService
+    {
+        public List<Course> courseList = new List<Course>();
+
+        public void Add(Course course)
+        {
+            courseList.Add(course);
+        }
+    }
+}
