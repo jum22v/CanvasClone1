cadf3dcd48cb11b3d6cd9f3a7fed04f0fcea0e8e
Juan Morales <juanesmoralesd@gmail.com>
2024-03-23 01:07:57 -0400

    Added FileItem Class

--- CanvasClone1/Library.CanvasClone1/Models/FileItem.cs
+++ CanvasClone1/Library.CanvasClone1/Models/FileItem.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace Library.CanvasClone1.Models
 {
-    public class ContentItem
+    public class FileItem : ContentItem
     {
-        public string? Name { set; get; }
-        public string? Description { get; set; }
-        
+        public string? Path { get; set; }
     }
 }
