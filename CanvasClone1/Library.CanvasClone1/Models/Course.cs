9acbc303cf85b4e34faafbe2f863507673299cff
Juan Morales <juanesmoralesd@gmail.com>
2024-03-26 16:39:00 -0400

    Added unique IDs for courses

--- CanvasClone1/Library.CanvasClone1/Models/Course.cs
+++ CanvasClone1/Library.CanvasClone1/Models/Course.cs
@@ -8,7 +8,19 @@ namespace Library.CanvasClone1.Models
 {
     public class Course
     {
-        public string Code { get; set; }
+        public string Code { 
+            get 
+            {
+                return $"{Prefix}{ID}";
+            } 
+        }
+
+        private static int lastId = 0;
+        public string Prefix { get; set; }
+        public int ID
+        {
+            get; private set;
+        }
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Person> Roster { get; set; }
@@ -16,12 +28,13 @@ namespace Library.CanvasClone1.Models
         public List<Module> Modules { get; set; }
         public Course()
         {    
-            Code = string.Empty;
             Name = string.Empty;
             Description = string.Empty;
             Roster = new List<Person>();
             Assignments = new List<Assignment>();
             Modules = new List<Module>();
+
+            ID = ++lastId;
         }
 
         public override string ToString()
