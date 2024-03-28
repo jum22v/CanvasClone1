3ec9fa6d20771df23101f2f16ec643a50c852a03
Juan Morales <juanesmoralesd@gmail.com>
2024-03-28 09:20:07 -0400

    migrating to MAUI and Adding Studing and instructor views

---
+++ CanvasClone1/MAUI.CanvasClone/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MAUI.CanvasClone.ViewModels;
+
+namespace MAUI.CanvasClone
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ CanvasClone1/MAUI.CanvasClone/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Library.CanvasClone1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI.CanvasClone.ViewModels
+{
+    internal class MainViewModel
+    {
+        public List<Person> Students { get; set; } = new List<Person>();
+    }
+}
