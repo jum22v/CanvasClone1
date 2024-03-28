5d2930338a2596c655df731be2cd488420d48216
Juan Morales <juanesmoralesd@gmail.com>
2024-03-28 09:54:03 -0400

    Added Instructor View and Student View again after lost save

---
+++ CanvasClone1/MAUI.CanvasClone/Views/InstructorView.xaml.cs
@@ -0,0 +1,17 @@
+using MAUI.CanvasClone.ViewModels;
+
+namespace MAUI.CanvasClone.Views;
+
+public partial class InstructorView : ContentPage
+{
+	public InstructorView()
+	{
+		InitializeComponent();
+		BindingContext = new InstructorViewViewModel();
+	}
+
+	private void CancelClicked(object sender, EventArgs e)
+	{
+		Shell.Current.GoToAsync("//MainPage");
+	}
+}
